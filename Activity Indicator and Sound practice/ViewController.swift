8be4f8bc1b532fc7cc850cb4a32d2ed6c6d2f246
Numan <abdullahalnuman235@gmail.com>
2020-01-06 22:19:28 -0800

    Initial Commit

---
+++ Activity Indicator and Sound practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Activity Indicator and Sound practice
+//
+//  Created by Abdullah Al Numan(AAN) on 1/6/20.
+//  Copyright © 2020 Abdullah Al Numan(AAN). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
